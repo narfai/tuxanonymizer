cfb1e4cf1b95859fd4a6738919aa3e9ad2bd9900
narfai <francois@azsystem.fr>
2020-01-12 19:43:07 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='tuxanonymizer',
   packages=['tuxanonymizer'],   # Chose the same as "name"
-  version='0.1',
+  version='0.2',
   license='MIT',
   description='Customer file obfuscation library and cli',
   author='Francois CADEILLAN',
   author_email='narfai@azsystem.fr',
   url='https://github.com/narfai/tuxanonymize',
-  download_url='https://github.com/narfai/tuxanonymize/archive/v_01.tar.gz',
+  download_url='https://github.com/narfai/tuxanonymize/archive/v_02.tar.gz',
   keywords=['ANONYMIZE', 'OBFUSCATION', 'CUSTOMER', 'XML'],
   install_requires=[
       'xmltodict',
